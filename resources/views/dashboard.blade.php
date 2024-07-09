28495271e5cd82a27ed60c46ac73135c576f7329
Chidozie Ugwu <jonasbraham@gmail.com>
2024-07-09 22:44:33 +0100

    implement forgot password and password reset

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,8 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                    {{-- Auth::user()->name --}}
+                    {{-- Auth::user()->email --}}
                 </div>
             </div>
         </div>
