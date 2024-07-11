a5e048c00c39dd5419906f604f3b42799edfa72c
Chidozie Ugwu <jonasbraham@gmail.com>
2024-07-11 12:08:25 +0100

    implement admin dashboard and logout functionality

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,25 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::controller(AdminController::class)->group(function(){
+    Route::get('/admin.logout', 'destroy')->name('admin.logout');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
