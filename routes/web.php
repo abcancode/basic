2c71850e9975d04b46be308da2a388beb022da0f
Chidozie Ugwu <jonasbraham@gmail.com>
2024-07-15 21:42:33 +0100

    implement frontend

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
 
 Route::get('/dashboard', function () {
